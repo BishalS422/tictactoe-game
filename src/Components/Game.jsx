25f8f6adec57061f39350ff95e92ad14752cc070
BishalS422 <sbishal422@gmail.com>
2022-02-16 22:31:50 +0545

    make layout for game

---
+++ src/Components/Game.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Bored from "./Bored";
+
+const Game = () => {
+  const status = "Next player is X";
+  const moves = (
+    <li>
+      {" "}
+      <button>Start the Game</button>
+    </li>
+  );
+  const squares = Array(9).fill(null);
+  return (
+    <div>
+      <div className="game-bored">
+        <Bored squares={squares}></Bored>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ul>{moves}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
